576d8dd94033210a0afc68eefdec695b6e345b2d
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-05 09:24:01 -0700

    Packed

--- pack/pac.ps1
+++ pack/pac.ps1
@@ -1,1 +1,2 @@
-
+iwr -useb "https://github.com/HimadriChakra12/wnvsh/raw/master/pack/pack.ps1" | iex
+iwr -useb "https://github.com/HimadriChakra12/wnvsh/raw/master/pack/packsconfig.ps1" | iex
--- pack/pack.ps1
+++ pack/pack.ps1
@@ -1,1 +1,3 @@
+iwr -useb "https://github.com/HimadriChakra12/wnvsh/raw/master/pack/choco-scoop.ps1" | iex
+iwr -useb "https://github.com/HimadriChakra12/wnvsh/raw/master/pack/winget.ps1" | iex
 
