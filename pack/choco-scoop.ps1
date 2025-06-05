741b089d66a2a1fa654835cea81c7a9c55aeb54a
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-05 09:14:35 -0700

    Pack Init

--- pack/choco-scoop.ps1
+++ pack/choco-scoop.ps1
@@ -1,1 +1,3 @@
-
+iwr -useb "https://community.chocolatey.org/install.ps1" | iex
+#scoop
+start-process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://github.com/HimadriChakra12/Himutil/raw/refs/heads/master/Him/scoop.ps1" | iex"'
--- pack/winget.ps1
+++ pack/winget.ps1
@@ -1,1 +1,15 @@
+#Winget
+start-process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "wsreset -i"' -wait -verb RunAs
+start-sleep -seconds 120
+switch ($read){
+    y{
+        start-process "https://apps.microsoft.com/detail/9nblggh4nns1?hl=en-US&gl=US"
+            $read = read-host "Winget got installed?[y]"
+            switch ($read){
+                y{
+                    Start-Process powershell -ArgumentList '-noexit -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://github.com/HimadriChakra12/Himutil/raw/refs/heads/master/Him/install.ps1" | iex"' -Verb RunAs -wait
+                }
+            }
+    }
+}
 
