45065c28142a5da1c85f3027e02a11e7cd2e4198
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-05 09:19:35 -0700

    pack config

--- pack/packsconfig.ps1
+++ pack/packsconfig.ps1
@@ -1,1 +1,21 @@
+#choco
+choco feature enable --name=allowGlobalConfirmation
+choco feature enable --name=useRememberedArgumentsForUpgrades
+choco feature enable --name=usePackageExitCodes
+choco feature enable --name=useEnhancedExitCodes
+choco feature enable --name=showDownloadProgress
+choco config set cacheLocation C:\choco-cache
+choco config set commandExecutionTimeoutSeconds 99999
+choco config set webRequestTimeoutSeconds 99999
+choco config set virusCheck false
+choco config set proxyCacheLocation C:\choco-cache
+#scoop
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add nerd-fonts
+scoop bucket add java
+scoop bucket add games   
 
+scoop update
+scoop upgrade --all
+scoop cleanup --all
