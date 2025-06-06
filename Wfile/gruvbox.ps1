6f84fbec730b6f34620825fc702a1d62b0742182
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-06 01:24:29 -0700

    Gruvboxized

--- Wfile/gruvbox.ps1
+++ Wfile/gruvbox.ps1
@@ -1,6 +1,12 @@
-iwr -uri "https://github.com/HimadriChakra12/Gruvbox-Windows/archive/refs/heads/main.zip" -OutFile "$env:TEMP/Gruvbox.zip" 
-expand-archive "$env:TEMP/Gruvbox.zip" "$env:TEMP/Gruvbox"
+$tmp = "$env:TEMP/Gruvbox.zip" 
+$tmpf = "$env:TEMP/Gruvbox" 
+if (-not(Test-path $tmp){
+iwr -uri "https://github.com/HimadriChakra12/Gruvbox-Windows/archive/refs/heads/main.zip" -OutFile "$tmp"
+}
+if (-not(Test-path $tmpf){
+expand-archive "$tmp" "$tmpf"
+}
 copy-item "$env:TEMP/Gruvbox/Gruvbox-Windows-main" "C:\Windows\Resources\Themes" -recurse 
 
 iwr -uri "https://github.com/HimadriChakra12/Gruvbox-Windows/releases/download/1.0.0/ThemeTool.exe" -OutFile "$env:TEMP/ThemeTool.exe" 
-start-process "$env:TEMP/ThemeTool.exe" 
+start-process "$env:TEMP/ThemeTool.exe"  -verb runas
