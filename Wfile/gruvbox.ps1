595c0adf1754d3d6b43972f1af0ff4d64b697c1c
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-08 10:04:11 -0700

    gruvbox

--- Wfile/gruvbox.ps1
+++ Wfile/gruvbox.ps1
@@ -6,9 +6,20 @@ iwr -uri "https://github.com/HimadriChakra12/Gruvbox-Windows/archive/refs/heads/
 if (-not(Test-path $tmpf)){
 expand-archive "$tmp" "$tmpf"
 }
-copy-item "$env:TEMP/Gruvbox/Gruvbox-Windows-main" "C:\Windows\Resources\Themes" -recurse 
-
-get-childitem -name "C:\Windows\Resources\Themes" 
+$idmpack = @(
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\Gruvbox " },
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\Gruvbox Dark.theme " },
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\Gruvbox Day.theme  " },
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\Gruvbox Night.theme" },
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\gruvlace Dark.theme " },
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\gruvlace Day.theme  " }
+    @{ path = "$env:TEMP/Gruvbox/Gruvbox-Windows-main\gruvlace Night.theme" }
+)
+# Copy each file to the IDM install directory
+foreach ($id in $idmpack) {
+    Copy-Item $id.path -Destination "C:\Windows\Resources\Themes" -Force
+}
+get-childitem "C:\Windows\Resources\Themes" -name
 
 iwr -uri "https://github.com/HimadriChakra12/Gruvbox-Windows/releases/download/1.0.0/ThemeTool.exe" -OutFile "$env:TEMP/ThemeTool.exe" 
 start-process "$env:TEMP/ThemeTool.exe"  -verb runas
