295d002bb5a7e094a0991c2051476eefdfdc879b
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-08 09:32:09 -0700

    idm

--- IDM/idm.ps1
+++ IDM/idm.ps1
@@ -9,5 +9,14 @@ start-process "$env:TEMP/DownloadManager.reg" -wait
 #Zip
 iwr -uri "https://github.com/HimadriChakra12/IDM-Crack/archive/refs/heads/main.zip" -OutFile "$env:TEMP/IDM.zip" 
 expand-archive "$env:TEMP/IDM.zip" "$env:TEMP/IDM"
-copy-item "$env:TEMP/IDM/IDM-Crack-main" "C:\Program Files (x86)\Internet Download Manager" -recurse 
+$idmpack = @(
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\GlobalErrors.log"}
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\idman.chm"}
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\IDMan.exe"}
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\IDMIECC.dll"}
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\license.txt"}
+@{ path = "C:\Users\himadri\AppData\Local\Temp\IDM\IDM-Crack-main\Uninstall.exe"}
+)
+foreach ($id in $idmpack){
+copy-item $id.path "C:\Program Files (x86)\Internet Download Manager" -recurse 
 start-process "C:\Program Files (x86)\Internet Download Manager\IDMan.exe"
