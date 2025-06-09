882377f227c91f92e0ac26e068e3b176e60c653c
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-09 03:11:38 -0700

    reg

--- Wfile/reg.ps1
+++ Wfile/reg.ps1
@@ -1,2 +1,2 @@
 iwr -uri "https://github.com/HimadriChakra12/wnvsh/releases/download/reg/combined.reg" -outfile "$env:temp/combined.reg"
-start-process "$env:temp/combined.reg" -verb runas
+start-process "$env:temp/combined.reg"
